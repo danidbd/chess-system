f16201afb26a4bf60afc8b848f2fbfa693a8a00a
daiel <dbrumcassimiro@gmail.com>
2023-06-09 11:35:26 -0300

    Placing pieces on the board

--- src/chess/ChessMach.java
+++ src/chess/ChessMach.java
@@ -1,12 +1,16 @@
 package chess;
 
 import bordgame.Bord;
+import bordgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMach {
     private Bord bord;
 
     public ChessMach(){
         bord = new Bord(8, 8);
+        initialSetup();
     }
     public ChessPiece[][] getPieces(){
         ChessPiece[][] mat = new ChessPiece[bord.getRow()][bord.getColumns()];
@@ -16,5 +20,9 @@ public class ChessMach {
             }
         }
         return mat;
+    }
+    private void initialSetup(){
+        bord.placePiece(new Rook(bord, Color.WHITE), new Position(2, 1));
+        bord.placePiece(new King(bord, Color.BLACK), new Position(0, 4));
     }
 }
