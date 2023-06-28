9c03b13d5eebd9f00a32b3b128bd4912662fd203
daiel <dbrumcassimiro@gmail.com>
2023-06-28 13:12:50 -0300

    Piece move count

--- src/chess/ChessMach.java
+++ src/chess/ChessMach.java
@@ -101,7 +101,8 @@ public class ChessMach {
     }
 
     private Piece makeMove(Position source, Position target){
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturedPice = board.removePiece(target);
         board.placePiece(p, target);
 
@@ -115,7 +116,8 @@ public class ChessMach {
     }
 
     private void undoMove(Position source, Position target, Piece capturedPiece){
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
 
         if (capturedPiece != null){
